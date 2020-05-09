31881fe81351cc3f4b1e179fde01ea7b46da0d03
chaewon0228 <s2033@e-mirim.hs.kr>
2020-05-09 23:50:55 +0900

    [Add] for문 반복 출력'

---
+++ step3/Norder_2741.java
@@ -0,0 +1,14 @@
+package step3;
+import java.util.Scanner;
+public class Norder_2741 {
+	public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1; i<=n; i++){
+           System.out.println(i);
+        }
+        System.out.println("\n");
+        sc.close();
+    }
+}
