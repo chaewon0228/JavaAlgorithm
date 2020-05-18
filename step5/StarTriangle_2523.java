c8cd9cc5631d03a6af0e527df9f6b350977fea02
chaewon0228 <s2033@e-mirim.hs.kr>
2020-05-18 22:45:29 +0900

    [Add]for문, Scanner(삼각형 별찍기)

---
+++ step5/StarTriangle_2523.java
@@ -0,0 +1,22 @@
+package step5;
+import java.util.Scanner;
+public class StarTriangle_2523 {
+	 public static void main(String args[]){
+	        Scanner sc = new Scanner(System.in);
+	        int n=sc.nextInt();
+	        for(int i=1; i<=n; i++) {
+	        	for(int j=0; j<i; j++) {
+	        		System.out.print("*");
+	        	}
+	        	System.out.println();
+	        }
+	        for(int i=n-1; i>0; i--) {
+	        	for(int j=0; j<i; j++) {
+	        		System.out.print("*");
+	        	}
+	        	System.out.println();
+	        }
+	        
+	        sc.close();
+	    }
+}
