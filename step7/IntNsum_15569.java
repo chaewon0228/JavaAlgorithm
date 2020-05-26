222c9dcf587f8b82fabb1b3f4e5177132c962a9c
chaewon0228 <s2033@e-mirim.hs.kr>
2020-05-26 22:17:26 +0900

    [Add]정수 N개의 합을 구하는 함수

---
+++ step7/IntNsum_15569.java
@@ -0,0 +1,12 @@
+package step7;
+
+public class IntNsum_15569 {
+	long sum(int[] a) {
+	    long ans = 0;
+	    for(int i= 0; i<a.length; i++){
+	        ans += a[i];
+	    }
+	    return ans;
+	}
+}
+
