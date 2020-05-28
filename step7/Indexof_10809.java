5a8e6e95248c838210f908cfe27e08e4714122ce
chaewon0228 <s2033@e-mirim.hs.kr>
2020-05-28 22:25:44 +0900

    [Add]문자열을 활용한 알파벳 찾기

---
+++ step7/Indexof_10809.java
@@ -0,0 +1,13 @@
+package step7;
+import java.util.Scanner;
+public class Indexof_10809 {
+	 public static void main(String args[]) {
+         Scanner sc= new Scanner(System.in);
+         String input= sc.next();
+         
+         for (char c= 'a'; c<='z'; c++) {
+                System.out.print(input.indexOf(c) + " ");
+         }
+         sc.close();
+   }
+}
