6a66bf1f8f85e8d3cdae539dbe82fd3b783b6ee7
chaewon0228 <s2033@e-mirim.hs.kr>
2020-05-30 23:06:07 +0900

    [Add]문자열을 활용한 단어의 개수 구하기

---
+++ step7/NumberOfWord_1152.java
@@ -0,0 +1,15 @@
+package step7;
+import java.util.Scanner;
+public class NumberOfWord_1152 {
+	public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String word= sc.nextLine().trim();
+        if(word.isEmpty()){
+            System.out.println(0);
+        } 
+        else{
+            System.out.println(word.split(" ").length);
+        }
+        sc.close();
+    }
+}
