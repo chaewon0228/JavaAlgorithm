1c9a23e3823ed9bc14ea1ed329f5517cfe7f64a4
chaewon0228 <s2033@e-mirim.hs.kr>
2020-06-02 21:56:38 +0900

    [Add]문자열: 각 문자를 반복하여 출력

---
+++ step8/StringRepeat_2675.java
@@ -0,0 +1,22 @@
+package step8;
+import java.util.Scanner;
+public class StringRepeat_2675 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            int cnt = sc.nextInt();
+            String str = sc.nextLine();
+ 
+            String result = "";
+            for (int j = 0; j < str.length(); j++) {
+                for (int k = 0; k < cnt; k++)
+                    result += str.charAt(j);
+            }
+            result = result.replaceAll(" ", "");
+            System.out.println(result);
+        }
+        sc.close();
+    }
+}
