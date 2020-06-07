efc6fcf6bf642a2dd0bfd841999566a0836d1d76
chaewon0228 <s2033@e-mirim.hs.kr>
2020-06-07 23:09:34 +0900

    [Add]호텔 방 번호의 규칙

---
+++ step9/Hotel_10250.java
@@ -0,0 +1,20 @@
+package step9;
+import java.util.Scanner;
+public class Hotel_10250 {
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+            int floor = N % H, room = N / H + 1;
+            if(N%H == 0) {
+                room = N / H;
+                floor = H;
+            }
+            System.out.println(floor*100+room);
+        } 
+        sc.close();
+    }
+}
