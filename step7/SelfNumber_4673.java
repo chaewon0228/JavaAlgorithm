924e1a2a1cb1c78ca07fd40610a1345fd46fa5d8
chaewon0228 <s2033@e-mirim.hs.kr>
2020-06-10 22:48:47 +0900

    [Add]함수 dn을 정의하여 생성자가 없는 숫자 찾기

---
+++ step7/SelfNumber_4673.java
@@ -0,0 +1,29 @@
+package step7;
+
+public class SelfNumber_4673 {
+	public static void main(String[] args){
+	      boolean[] isNotSelfNumber = new boolean[10001];
+	         
+	        for (int i = 1; i <= 10000; ++i) {
+	            int dn = getDn(i);
+	            if (dn <= 10000) {
+	                isNotSelfNumber[dn] = true;             
+	            }
+	        }
+	         
+	        for (int i = 1; i < isNotSelfNumber.length; ++i) {
+	            if (!isNotSelfNumber[i]) {
+	                System.out.println(i);              
+	            }
+	        }
+	    }
+	     
+	    private static int getDn(int n) {
+	        int dn = n;
+	        while(n > 0) {
+	            dn += n % 10;
+	            n /= 10;
+	        }
+	        return dn;
+	    }
+}
