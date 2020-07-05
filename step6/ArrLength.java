e32cb157b9bb652b50ab6a5fd87e4997d7c84fd3
chaewon0228 <s2033@e-mirim.hs.kr>
2020-07-05 22:01:39 +0900

    [Add] 2차원 배열의 이해

---
+++ step6/ArrLength.java
@@ -0,0 +1,23 @@
+package step6;
+
+public class ArrLength {
+	public static void main(String[] args) {
+		
+		int[][] arr = new int[2][3];
+		int a= 10;
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = a; 
+				a += 10;
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
