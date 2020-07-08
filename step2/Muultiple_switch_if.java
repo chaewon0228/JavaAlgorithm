7fa2ced7e4b218560e0c8683bdcf7f516b291f5c
chaewon0228 <s2033@e-mirim.hs.kr>
2020-07-08 22:26:08 +0900

    [Add] 양의 정수를 입력한 후 switch와 if문을 사용하여 배수찾기

---
+++ step2/Muultiple_switch_if.java
@@ -0,0 +1,21 @@
+package step2;
+import java.util.Scanner;
+public class Muultiple_switch_if {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("수 입력 : ");
+		int number = sc.nextInt();
+		sc.close();
+		switch(number % 3){
+			case 0 : System.out.println(number + "은 3의 배수이다."); break;
+			default : switch(number % 5){
+				 case 0 : System.out.println(number + "은 5의 배수이다."); break;
+				 default : switch(number % 8){		
+					  case 0 : System.out.println(number+"은 8의 배수이다."); break;
+					  default : System.out.println("어느 배수도 아니다.");
+	                         		 
+	         		           }                  
+	             }      
+	  }
+	}
+}
