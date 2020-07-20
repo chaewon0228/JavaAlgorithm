7056c34047e5571d370d361f94fe5ecd2b37d02b
chaewon0228 <s2033@e-mirim.hs.kr>
2020-07-20 21:59:22 +0900

    [Add] while문: 각 자릿수의 합계 구하기

---
+++ step10/WhileNumSum.java
@@ -0,0 +1,17 @@
+package step10;
+import java.util.Scanner;
+public class WhileNumSum {
+	public static void main(String[] args) {
+       System.out.print("숫자를 입력하세요 : ");
+           Scanner sc = new Scanner(System.in);
+           int num = 0, sum = 0; 
+           num = sc.nextInt(); sc.close();
+           
+           while(num!=0){
+              sum += num%10;
+              num /= 10;
+           }
+           System.out.println("각 자리수의 합은 "+sum+"입니다.");
+           
+   }
+}
