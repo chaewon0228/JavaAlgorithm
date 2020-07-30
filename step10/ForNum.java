f679720bfb31a94bc31c8bd6e7930fa60c20e88d
chaewon0228 <s2033@e-mirim.hs.kr>
2020-07-30 22:54:58 +0900

    [Add] for문: 수를 입력받아 1000의 자리부터 출력하기

---
+++ step10/ForNum.java
@@ -0,0 +1,14 @@
+package step10;
+import java.util.Scanner;
+public class ForNum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("데이터 입력 : ");
+		int a = sc.nextInt();
+		sc.close();
+		
+		for(int i=1000; i>=1; i/=10) {
+			System.out.println(i+"의 자리 : "+(a/i)%10);
+		}
+	}
+}
