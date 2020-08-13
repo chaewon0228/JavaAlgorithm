7aa4042c165c1e9b5ec4b2d4eed080e3c5649104
chaewon0228 <s2033@e-mirim.hs.kr>
2020-08-13 23:18:29 +0900

    [Add] 각 사람의 몸무게와 키를 비교하여 덩치 등수 매기기

---
+++ step13/HeightWeight_7568.java
@@ -0,0 +1,24 @@
+package step13;
+import java.util.Scanner;
+public class HeightWeight_7568 {
+	public static void main(String[] args){
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        int xy[][] = new int [n][2];
+	        int rank [] = new int [n];
+	        
+	        for(int i=0; i<n; i++){
+	            xy[i][0] = sc.nextInt();
+	            xy[i][1] = sc.nextInt();
+	            rank[i] = 1;
+	        }
+	        for(int i=0; i<n; i++){
+	            for(int j=0; j<n; j++){
+	                if(xy[i][0] > xy[j][0] && xy[i][1] > xy[j][1]) rank[j]++;
+	            }
+	        }
+	        for(int i=0; i<n; i++) System.out.print(rank[i]+" ");
+	        sc.close();
+	}
+}
+
