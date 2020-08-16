abadd1d7f2c0ac3ebef7542b8b5fa067090405ac
chaewon0228 <s2033@e-mirim.hs.kr>
2020-08-16 22:30:57 +0900

    [추가] M의 분해 합 인 N을 입력하고 M의 최솟값 구하기

--- step13/분해합_2231.java
+++ step13/분해합_2231.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 public class ºÐÇØÇÕ_2231 {
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); sc.close();
+        int n = sc.nextInt(); 
+        
         int size = String.valueOf(n).length();
-        int start = n-(9*size); 
+        int start = n - (9 * size); 
         int result = 0;
         
         for(int i=start; i<n; i++){
-            int sum = i;
+            
+        	int sum = i;
             int j = i;
+           
             while(j>0){
                 sum += j%10;
                 j /= 10;
             }
+            
             if(sum == n){
                 result = i;
                 break;
             }
         }
+       
         System.out.println(result);
+        sc.close();
 	}
 }
 
