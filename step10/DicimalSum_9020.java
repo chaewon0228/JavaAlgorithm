c9e93c97b048582b2f4e39b80775cbd1263f0441
chaewon0228 <s2033@e-mirim.hs.kr>
2020-08-18 23:57:17 +0900

    [Add] 2보다 큰 짝수를 입력하여 두 소수의 합 출력하기

--- step10/DicimalSum_9020.java
+++ step10/DicimalSum_9020.java
@@ -4,6 +4,7 @@ public class DicimalSum_9020 {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int a = 0, b = 0; 
+		
 		System.out.print("2이상 짝수 개수 입력 : ");
 		int t = sc.nextInt();
 
@@ -21,6 +22,7 @@ public class DicimalSum_9020 {
 		}
 		sc.close();
 	}
+	
 	public static boolean check(int n){
 		for(int i=2; i<=n/2; i++){
 			if(n%i == 0) return false;
