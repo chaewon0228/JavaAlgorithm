1a34f3543eab196d310b7333bf0c7d267d523e81
chaewon0228 <s2033@e-mirim.hs.kr>
2021-02-20 23:36:24 +0900

    [Add]람다식) 좌표 정렬하기

---
+++ step12/PointSort_11650.java
@@ -0,0 +1,26 @@
+package step12;
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+public class PointSort_11650 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][2];
+		
+		for(int i=0; i<n; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) return e1[1] - e2[1];
+			else return e1[0] - e2[0];
+		});
+		
+		for(int i=0; i<n; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+		sc.close();
+	}
+}
