6b8f0b0c0d12e92848b9bc6f7b595f065d7e59a1
chaewon0228 <58872725+chaewon0228@users.noreply.github.com>
2021-03-03 23:42:14 +0900

    [Add] if문) 두 정수 크기 비교하기

--- step2/AcompareB_1330.java
+++ step2/AcompareB_1330.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 
 public class AcompareB_1330 {
 	public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int a,b;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        if(a>b)System.out.println(">");
-        else if(a<b)System.out.println("<");
-        else System.out.println("==");
-        sc.close();
-    }
+        	Scanner sc = new Scanner(System.in);
+        	int a = sc.nextInt();
+        	int b = sc.nextInt();
+		
+        	if(a > b) System.out.println(">");
+        	else if(a < b) System.out.println("<");
+        	else System.out.println("==");
+        }
 }
