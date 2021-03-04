d74e6e100d8925be1c9ef1b83323e825e6a7b9e7
chaewon0228 <58872725+chaewon0228@users.noreply.github.com>
2021-03-04 23:15:41 +0900

    [Add] 알람 시간 구하기

--- step2/Alarm_2884.java
+++ step2/Alarm_2884.java
@@ -2,19 +2,16 @@ package step2;
 import java.util.Scanner;
 public class Alarm_2884 {
 	public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        int H,M;
-        H=sc.nextInt();
-        M=sc.nextInt();
-        if(M<45){
-            H--;
-            M=60-(45-M);
-            if(H<0){
-                H=23;
-            }
-            System.out.println(H+" "+M);
-        }
-        else System.out.println(H+" "+(M-45));
-        sc.close();
-    }
+        	Scanner sc = new Scanner(System.in);
+        	int H = sc.nextInt();
+        	int M = sc.nextInt();
+        	if(M < 45){
+            		H--;
+            		M = 60-(45-M);
+            		if(H<0) H = 23;
+            		System.out.println(H + " " + M);
+        	}
+        	else System.out.println(H + " " + (M - 45));
+        	sc.close();
+    	}
 }
